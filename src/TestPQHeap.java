b997ff88b52e5b60583e7e346e669e2bad2563bf
parthpatel <parth2274@gmail.com>
2023-04-10 11:52:58 -0400

    11:52am

--- src/TestPQHeap.java
+++ src/TestPQHeap.java
@@ -4,10 +4,8 @@ public class TestPQHeap {
 	public static void main(String[]args){
 		Scanner scan = new Scanner(System.in);
 
-		PQ_heap<?> pq = null;
 
 		int choice;
-		String type = "";
 
 		do {
 			System.out.println("""
@@ -26,36 +24,8 @@ public class TestPQHeap {
 			choice = scan.nextInt();
 
 			switch (choice){
-				case 0: {
-					System.out.println("Enter queue type (integer or string): ");
-					type = scan.next();
-					type = type.toLowerCase();
-					if (type.equals("integer")){
-						pq = new PQ_heap<Integer>();
-					}
-					else if (type.equals("string")){
-						pq = new PQ_heap<String >();
-					}
-					else {
-						System.out.println("Invalid queue type.");
-					}
-					break;
-				}
-				case 1: //check if queue has been created
-					if (type.equals("integer")){
-						System.out.println("Enter integer to enqueue: ");
-						int num = scan.nextInt();
-						pq.enqueue(num);
-					}
-					else if (type.equals("string")){
-						System.out.println("Enter string to enqueue: ");
-						String str = scan.next();
-						pq.enqueue(str);
-					}
-					else {
-						System.out.println("Queue not created.");
-					}
-					break;
+				case 0:
+
 
 			}
 
